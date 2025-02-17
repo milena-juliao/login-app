8bd39fd5295edf35f0816dd0ff6a652e4566cfd5
Milena Julião <milenadjuliao@gmail.com>
2025-02-17 17:09:55 -0300

    rotas criadas

---
+++ backend/src/routes.js
@@ -0,0 +1,83 @@
+const jwt = require("jsonwebtoken");
+const { Router } = require("express");
+
+const routes = Router();
+
+const users = [
+    {
+        name: "admin",
+        email: "admin@spsgroup.com.br",
+        password: "1234",
+        type: "admin",
+    },
+];
+
+const findUserByEmail = (email) => users.find((user) => user.email === email);
+
+const authenticateToken = (req, res, next) => {
+    const token = req.headers["authorization"];
+    if (!token) return res.status(403).json({ message: "Token é necessário!" });
+
+    jwt.verify(token, "secretkey", (err, user) => {
+        if (err) return res.status(403).json({ message: "Token inválido!" });
+        req.user = user;
+        next();
+    });
+};
+
+//rotas
+
+routes.post("/login", (req, res) => {
+    const { email, password } = req.body;
+    const user = findUserByEmail(email);
+    if (user && user.password === password) {
+        const token = jwt.sign(
+            { email: user.email, type: user.type },
+            "secretkey",
+            { expiresIn: "1h" }
+        );
+        return res.json({ token });
+    } else {
+        return res
+            .status(401)
+            .json({ message: "Informações de login incorretas!" });
+    }
+});
+
+routes.post("/users", authenticateToken, (req, res) => {
+    const { email, name, type, password } = req.body;
+    if (findUserByEmail(email)) {
+        return res.status(400).json({ message: "Email já cadastrado!" });
+    }
+
+    const newUser = { email, name, type, password };
+    users.push(newUser);
+    res.status(201).json(newUser);
+});
+
+routes.delete("/users/:email", authenticateToken, (req, res) => {
+    const { email } = req.params;
+    const index = users.findIndex((user) => user.email === email);
+    if (index === -1)
+        return res.status(404).json({ message: "Usuário não encontrado!" });
+
+    users.splice(index, 1);
+    res.status(200).json({ message: "Usuário removido com sucesso!" });
+});
+
+routes.put("/users/:email", authenticateToken, (req, res) => {
+    const { email } = req.params;
+    const { name, type, password } = req.body;
+
+    const user = findUserByEmail(email);
+    if (!user)
+        return res.status(404).json({ message: "Usuário não encontrado!" });
+
+    user.name = name || user.name;
+    user.type = type || user.type;
+    user.password = password || user.password;
+
+    res.status(200).json(user);
+});
+
+module.exports = routes;
